a9b2871387a9fc0011116759b29849e191e37934
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-05-07 13:35:24 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const petsData = [
+  {
+    name: "Purrsloud",
+    species: "Cat",
+    favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+    birthYear: 2017,
+    photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
+  },
+  {
+    name: "Barksalot",
+    species: "Dog",
+    birthYear: 2008,
+    photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
+  },
+  {
+    name: "Meowsalot",
+    species: "Cat",
+    favFoods: ["tuna", "catnip", "celery"],
+    birthYear: 2012,
+    photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
+  }
+];
+
+document.getElementById("app").innerHTML = `Hello, two plus two equas ${2 + 2} and it always will.
+<p> hi </p>
+<p>hello</p>
+`
