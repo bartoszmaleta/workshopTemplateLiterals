ce00992a553cfdcee9d3d9a1a9d75eda84861322
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-05-07 14:03:21 +0200

    Add cards + title + species + age

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const petsData = [
     name: "Purrsloud",
     species: "Cat",
     favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
-    birthYear: 2017,
+    birthYear: 2020,
     photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
   },
   {
@@ -16,7 +16,7 @@ const petsData = [
     name: "Meowsalot",
     species: "Cat",
     favFoods: ["tuna", "catnip", "celery"],
-    birthYear: 2012,
+    birthYear: 2019,
     photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
   }
 ];
@@ -26,6 +26,29 @@ const petsData = [
 // <p>hello</p>
 // `
 
+function age(birthYear) {
+  let calculatedAge = new Date().getFullYear() - birthYear;
+  if (calculatedAge < 1) {
+    return 'baby';
+  } else if (calculatedAge == 1) {
+    return '1 year old';
+  } else {
+    return `${calculatedAge} years old`;
+  }
+}
+
+function petTemplate(pet) {
+  return `
+  <div class="animal">
+    <img class="pet-photo" src="${pet.photo}">
+    <h2 class="pet-name">${pet.name} <span class="species">(${pet.species})</span></h2>
+    <p><strong>Age: ${age(pet.birthYear)}</strong></p>
+  </div>
+  `
+}
+
 document.getElementById("app").innerHTML = `
 <h1 class="app-title">Pets (${petsData.length} results)</h1>
+${petsData.map(petTemplate).join('')}
+<p class="footer">These ${petsData.length} pets were added recently. Check back soon for updates.</p>
 `
